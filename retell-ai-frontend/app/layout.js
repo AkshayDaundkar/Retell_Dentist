e6a20d62b0bee2f1c992938630da3e6c67f893ed
Akshay Daundkar <akshaydaundkar01@gmail.com>
2025-07-14 21:46:20 -0500

    Added backend and Frontend for my Model

---
+++ retell-ai-frontend/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Meet Akshay - The Future Is Now',
+  description: 'Meet Akshay - The Future Is Now',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
